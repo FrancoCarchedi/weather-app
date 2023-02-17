aaaba6c53124a7675beaef89afb57637e1ff8ba2
Franco <50966166+FrancoCarchedi@users.noreply.github.com>
2023-02-17 12:11:02 -0300

    fix: day icon height fix

--- src/components/Day.jsx
+++ src/components/Day.jsx
@@ -3,7 +3,7 @@ export const Day = ({date, max_temp, min_temp, units, img}) => {
   return (
     <div className="rounded flex flex-col items-center bg-background-800">
       <h1 className="font-medium text-base text-center my-4">{date}</h1>
-      <img className="w-14 m-0" src={img} alt=""/>
+      <img className="h-16 m-0" src={img} alt=""/>
       <div className="flex flex-row my-4">
         <p className="font-medium text-base mr-2">{max_temp + units}</p>
         <p className="font-medium text-base ml-2 text-white-300">{min_temp + units}</p>
